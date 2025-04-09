4f845856ea101e8b5631ca075eebd2731861dd0b
Acker404 <mark921202@gmail.com>
2025-04-10 02:14:02 +0800

    init

---
+++ GovernmentOpenDataAnalysis/Class1.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using CsvHelperApplication;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GovernmentOpenDataAnalysis
+{
+    internal class Class1
+    {
+    }
+}
+
+namespace CsvHelperApplication
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ GovernmentOpenDataAnalysis/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+//https://dog0416.blogspot.com/2019/11/aspnet-core-csvhelper-c-csv.html#google_vignette
+Console.WriteLine("Hello, World!");
